03697dc5f2b2525c2a3a0d781e45da4371010c77
zFranco23 <francoheflo@gmail.com>
2021-12-21 13:38:00 -0500

    First commit

---
+++ routes/api.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var router = express.Router();
+var Beer = require('../models/beer');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express', message: 'Hello Worlds Out There!' });
+});
+
+router.route('/beers')
+    .post(function(req, res) {
+        var beer = new Beer();
+        beer.name = req.body.name;
+        
+        beer.save(function(err) {
+            if (err)
+                res.send(err);
+                
+            res.json({message: 'Beer Created'});
+        });
+    
+    
+    })
+    
+    .get(function(req, res) {
+        Beer.find(function(err, beers) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(beers);
+        });
+    });
+    
+
+router.route('/beers/:beer_id')
+    .get(function(req, res) {
+        Beer.findById(req.params.beer_id, function(err, beer) {
+            if (err) {
+              res.send(err);
+            }
+            res.json(beer);
+        });
+    
+    })
+    
+    .put(function(req, res) {
+        Beer.findById(req.params.beer_id, function(err, beer) {
+            if (err) {
+              res.send(err);
+            }
+            
+            beer.name = req.body.name;
+            beer.save(function(err) {
+                if (err) {
+                  res.send(err);
+                }
+                res.json({message: 'Beer Updated'})
+            });
+            
+        });
+    })
+    
+    .delete(function(req, res) {
+        Beer.remove({_id:req.params.beer_id}, function(err) {
+            if (err) {
+              res.send(err);
+            }
+            res.json({message: 'Successfully Deleted'})
+        })
+    });
+
+
+
+module.exports = router;
